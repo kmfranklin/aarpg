b59cd7992c7c698afdff5e825c7e7425c62d7215
Kevin Franklin <kevin@franklindesign.io>
2025-05-06 20:02:38 -0400

    feat: Add player attack
    - Update player sprite sheet
    - Script attack animations
    - Add player Attack state
    - Implement player sprite shadow

--- Player/Scripts/state_idle.gd
+++ Player/Scripts/state_idle.gd
@@ -1,6 +1,7 @@
 class_name State_Idle extends State
 
 @onready var walk : State = $"../Walk"
+@onready var attack: State_Attack = $"../Attack"
 
 ## What happens when the player enters this State?
 func Enter() -> void:
@@ -24,4 +25,6 @@ func Physics(_delta : float) -> State:
 
 ## What happens with input events in this state?
 func HandleInput(_event : InputEvent) -> State:
+	if _event.is_action_pressed("attack"):
+		return attack
 	return null
--- Player/Scripts/state_walk.gd
+++ Player/Scripts/state_walk.gd
@@ -3,6 +3,7 @@ class_name State_Walk extends State
 @export var move_speed : float = 100.0
 
 @onready var idle : State = $"../Idle"
+@onready var attack: State_Attack = $"../Attack"
 
 ## What happens when the player enters this State?
 func Enter() -> void:
@@ -31,4 +32,6 @@ func Physics(_delta : float) -> State:
 
 ## What happens with input events in this state?
 func HandleInput(_event : InputEvent) -> State:
+	if _event.is_action_pressed("attack"):
+		return attack
 	return null
